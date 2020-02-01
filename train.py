1369d2effa268fb1558e8cca2369047af42e483c
dungpham98 <phamtuandung12@gmail.com>
2020-02-01 08:46:56 +0700

    Update Readme

--- train.py
+++ train.py
@@ -10,6 +10,35 @@ from options import *
 from model.hidden import Hidden
 from average_meter import AverageMeter
 
+def cropImg(size,img_tensor):
+    imgs=[]
+    batch = int(img_tensor.shape[0])
+    channel = int(img_tensor.shape[1])
+    h = int(img_tensor.shape[2])
+    w = int(img_tensor.shape[3])
+    n = int(h/size)
+    i = 0
+    while(i*size < h):
+        j = 0
+        while(j*size < w):
+            i_n =int(i*size)
+            j_n = int(j*size)
+            img = img_tensor[0:batch,0:channel,i_n:(i_n+size),j_n:(j_n+size)]
+            imgs.append(img)
+            #torchvision.utils.save_image(img,"cropped"+str(i_n)+str(j_n)+".jpg")
+            j = j + 1 
+        i = i + 1
+    return imgs
+
+def concatImgs(imgs):
+    img_len = len(imgs)
+    i = 0
+    img_cat =[]
+    while(i < 16):
+        img_cat.append(torch.cat([imgs[0+i],imgs[1+i],imgs[2+i],imgs[3+i]],dim=3))
+        i = i + 4
+    img = torch.cat([img_cat[0],img_cat[1],img_cat[2],img_cat[3]],2)
+    return img
 
 def train(model: Hidden,
           device: torch.device,
@@ -46,13 +75,37 @@ def train(model: Hidden,
         training_losses = defaultdict(AverageMeter)
         epoch_start = time.time()
         step = 1
+        #train
         for image, _ in train_data:
             image = image.to(device)
+            """
             message = torch.Tensor(np.random.choice([0, 1], (image.shape[0], hidden_config.message_length))).to(device)
             losses, _ = model.train_on_batch([image, message])
+            print(losses)
+            """
+            #crop imgs
+            imgs = cropImg(32,image)
+            #iterate img
+            bitwise_arr=[]
+            main_losses = None
+            for img in imgs:
+                img=img.to(device)
+                message = torch.Tensor(np.random.choice([0, 1], (img.shape[0], hidden_config.message_length))).to(device)
+                losses, _ = model.train_on_batch([img, message])
+                main_losses = losses
+                for name, loss in losses.items():
+                    if(name == 'bitwise-error  '):
+                        bitwise_arr.append(loss)
 
-            for name, loss in losses.items():
-                training_losses[name].update(loss)
+            bitwise_arr = np.array(bitwise_arr)
+            bitwise_avg = np.average(bitwise_arr)
+
+            for name, loss in main_losses.items():
+                if(name == 'bitwise-error  '):
+                    training_losses[name].update(bitwise_avg)
+                else:
+                    training_losses[name].update(loss)    
+
             if step % print_each == 0 or step == steps_in_epoch:
                 logging.info(
                     'Epoch: {}/{} Step: {}/{}'.format(epoch, train_options.number_of_epochs, step, steps_in_epoch))
@@ -72,12 +125,37 @@ def train(model: Hidden,
         first_iteration = True
         validation_losses = defaultdict(AverageMeter)
         logging.info('Running validation for epoch {}/{}'.format(epoch, train_options.number_of_epochs))
+
+        #val
         for image, _ in val_data:
             image = image.to(device)
-            message = torch.Tensor(np.random.choice([0, 1], (image.shape[0], hidden_config.message_length))).to(device)
-            losses, (encoded_images, noised_images, decoded_messages) = model.validate_on_batch([image, message])
-            for name, loss in losses.items():
-                validation_losses[name].update(loss)
+            #crop imgs
+            imgs = cropImg(32,image)
+            #iterate img
+            bitwise_arr=[]
+            main_losses = None
+            encoded_imgs=[]
+            for img in imgs:
+                img=img.to(device)
+                message = torch.Tensor(np.random.choice([0, 1], (img.shape[0], hidden_config.message_length))).to(device)
+                losses, (encoded_images, noised_images, decoded_messages) = model.validate_on_batch([img, message])
+                encoded_imgs.append(encoded_images)
+                main_losses = losses
+                for name, loss in losses.items():
+                    if(name == 'bitwise-error  '):
+                        bitwise_arr.append(loss)
+
+            bitwise_arr = np.array(bitwise_arr)
+            bitwise_avg = np.average(bitwise_arr)
+
+            for name, loss in main_losses.items():
+                if(name == 'bitwise-error  '):
+                    validation_losses[name].update(bitwise_avg)
+                else:
+                    validation_losses[name].update(loss)
+            #concat image
+            encoded_images = concatImgs(encoded_imgs)
+
             if first_iteration:
                 if hidden_config.enable_fp16:
                     image = image.float()
